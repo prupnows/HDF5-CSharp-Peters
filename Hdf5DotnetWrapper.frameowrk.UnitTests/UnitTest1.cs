da4b5f04f598eab67e922af0354ccdb650bdca8b
Lior Banai <liorbanai@gmail.com>
2020-01-29 18:07:40 +0200

    unit tests?

---
+++ Hdf5DotnetWrapper.frameowrk.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hdf5DotnetWrapper.frameowrk.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
