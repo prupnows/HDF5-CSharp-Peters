ebf2c8a8c98949482cc0d918457e8ab9756b299c
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2020-04-24 16:09:57 +0300

    update ci settings

--- HDF5-CSharp.UnitTests/FilesUnitTests.cs
+++ HDF5-CSharp.UnitTests/FilesUnitTests.cs
@@ -7,16 +7,20 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace HDF5CSharp.UnitTests.Core
 {
     [TestClass]
-    public class FilesUnitTests:Hdf5BaseUnitTests
+    public class FilesUnitTests : Hdf5BaseUnitTests
     {
         [TestMethod]
         public void TestReadStructure()
         {
             string fileName = @"FileStructure.h5";
-            var tree =Hdf5.ReadTreeFileStructure(fileName);
-            var flat = Hdf5.ReadFlatFileStructure(fileName);
-            File.Delete(fileName);
-            Assert.IsFalse(File.Exists(fileName));
+            if (File.Exists(fileName))
+            {
+                var tree = Hdf5.ReadTreeFileStructure(fileName);
+                var flat = Hdf5.ReadFlatFileStructure(fileName);
+                File.Delete(fileName);
+                Assert.IsFalse(File.Exists(fileName));
+            }
         }
     }
 }
+
